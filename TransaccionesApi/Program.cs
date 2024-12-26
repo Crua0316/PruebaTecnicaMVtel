0fa902ff1425385ee6b4e30cd682c0e4b5e6ea6b
unknown <Mvtel@HV-00053.MVTEL.CORP>
2024-12-26 12:23:27 -0500

    Fix errors

--- TransaccionesApi/Program.cs
+++ TransaccionesApi/Program.cs
@@ -1,20 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar servicios
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // Necesario para Swagger
-builder.Services.AddSwaggerGen(); // Agregar Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configurar Swagger solo para desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Configurar middlewares
-app.MapControllers(); // Configurar rutas
+app.MapControllers();
 
 app.Run();
